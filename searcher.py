60b8eead15b65fefcee7a36be5f39d2668826242
Khasan khamraev <khasanknyazev81@gmail.com>
2024-03-14 15:53:44 +0300

    create number validator

--- searcher.py
+++ searcher.py
@@ -1,8 +1,13 @@
 try:
     import requests
     from art import tprint
+    from time import sleep
     import re
     import os
+    import phonenumbers
+    from phonenumbers import carrier
+    from phonenumbers import geocoder
+    from phonenumbers import timezone
 except ModuleNotFoundError:
     print('Установлены не все модули..')
     exit()
@@ -20,7 +25,7 @@ def main():
     
     tprint("searcher", space=1)
     print("Выберите варинты поиска")
-    print("(1) Поиск по id\n(2) Поиск по номеру")
+    print("(1) Поиск по nick\n(2) Поиск по номеру\n(3) Выход")
     num = int(input("--> "))
     
     if num == 1:
@@ -30,10 +35,31 @@ def main():
     elif num == 2:
         os.system("cls")
         tprint("searcher", space=1)
-        number = re.sub("(?:)?(?:[^[0-9]*)", "", input("number  ->"))
-    else:
+        try:
+            phone = re.sub("(?:\+)?(?:[^[0-9]*)", "", input("number ->"))
+            PhoneNumberObject = phone
+
+            #+792579ххххх
+            #Country Code: 7 National Number: 92579ххххх
+            #+7 925 79х-хх-хх
         
+            FormattedPhoneNumber = "+" + phone
+            PhoneNumberObject = phonenumbers.parse(FormattedPhoneNumber, None)
+            local_international_format = phonenumbers.format_number(PhoneNumberObject, phonenumbers.PhoneNumberFormat.INTERNATIONAL)
+            print(1)
+        except:
+            print('\033[1m\033[31mНомер телефона введён неверно!\033[0m')
+            sleep(2)
+            main()
+    elif num == 3:
+        exit()
+    else:
+        os.system("cls")
+        tprint("searcher", space=1)
+        print("невернно ведены данные")
+        sleep(2)
+        main()
 
-
+       
 if __name__ == "__main__":
     main()
